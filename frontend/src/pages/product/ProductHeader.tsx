312002e64656ae3819c8353a7973f71b7b5436bd
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-08 00:02:32 +0530

    Responsive the Product Page Detail Section

--- frontend/src/pages/product/ProductHeader.tsx
+++ frontend/src/pages/product/ProductHeader.tsx
@@ -1,12 +1,12 @@
 const ProductHeader = () => {
   return (
-    <div>
-      <h1 className="text-6xl font-semibold font-nunito leading-[38.4px] mb-3">
+    <div className="px-3 md:px-0">
+      <h1 className="text-3xl md:text-6xl font-semibold font-nunito leading-[38.4px] mb-3">
         Anchor Full Cream Fresh Milk 1L
       </h1>
-      <div className="flex flex-row items-center gap-4">
+      <div className="flex items-center gap-4 md:flex-row">
         <p className="w-[120px] h-[20px]">⭐⭐⭐⭐⭐</p>
-        <span className="text-sm font-normal leading-[16.8px] text-customGreen text-center">
+        <span className="text-sm font-normal leading-[16.8px] text-customGreen">
           4 Customer Review
         </span>
       </div>
