e611ba1a0b3440644aa88697631df7e456feb9ee
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-15 03:56:48 +0530

    responsive design of bottom bar

--- frontend/src/components/common/bottomBar.tsx
+++ frontend/src/components/common/bottomBar.tsx
@@ -1,14 +1,34 @@
+import { TiLocation } from "react-icons/ti";
+import { IoCall } from "react-icons/io5";
+
 const BottomBar = () => {
   return (
-    <div className="font-lato text-[12px] h-[45px] w-full bg-[#09965D] rounded-t-[20px] pt-4">
-      <div className="flex max-w-[1120px] mx-auto justify-between">
-        <div>
-          <p className="text-white">© Copyright 2023. by A65 GROUP TM</p>
+    <div className="h-8 text-xs xs:h-14 w-full bg-[#09965D] rounded-t-[20px] px-[20px] flexCenter gap-12 lg:gap-32 mx-auto xs:text-base lg:px-[100px] xl:px-[160px] mb-[50px] sm:mb-0">
+      <p className="text-white">© Copyright 2023. by A65 GROUP TM</p>
+      <div className="hidden xxl:block">
+        <div className="hidden xs:block">
+          <div className="flex items-center">
+            <p className="hidden md:block">Delivery from: </p>
+            <span className="mx-1 opacity-50">
+              <TiLocation />
+            </span>
+            <span className="text-white">64 Front Street, 11, Colombo...</span>
+          </div>
         </div>
-        <p className="text-white">Address: 64 Front Street, 11, Colombo...</p>
-        <div>
-          <p className="text-white">Call: ( 011) 2696523</p>
+        <p className="cursor-pointer xs:hidden text-white/90 hover:scale-110">
+          <TiLocation />
+        </p>
+      </div>
+      <div className="hidden md:block">
+        <div className="flex-row flexCenter">
+          <p className="hidden md:block">
+            Call Us for any query or help:&nbsp;
+          </p>
+          <span className="hidden text-white xs:block">(011) 2696523</span>
         </div>
+        <p className="cursor-pointer hover:scale-110 xs:hidden text-white/90">
+          <IoCall />
+        </p>
       </div>
     </div>
   );
