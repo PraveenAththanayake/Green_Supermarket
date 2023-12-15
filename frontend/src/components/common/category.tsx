85a7746bdbf60f8bd01fc6c7d98448b80ae53291
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-15 07:52:27 +0530

    Responsive design for category section

--- frontend/src/components/common/category.tsx
+++ frontend/src/components/common/category.tsx
@@ -6,21 +6,21 @@ const Category = () => {
     <section className="max-w-[1100px] my-[50px] mx-auto mt-[100px] mb-[200px] px-3">
       <div className="mx-5 flexBetween">
         <h2 className="h2-bold">Category</h2>
-        <span className="text-[15px] font-semibold text-gray tracking-[0.5px] leading-[18px]">
+        <span className="text-xs xs:text-[15px] font-semibold text-gray tracking-[0.5px] leading-[18px]">
           View all products
         </span>
       </div>
-      <div className="grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-1 gap-y-3 md:gap-x-5 md:gap-y-10 mt-11">
+      <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-1 gap-y-5 md:gap-x-5 md:gap-y-10 mt-11">
         {CategoriesList.map((category) => (
           <div key={category.id} className="flexCenter">
             <Link to={`/category/${category.id}`}>
-              <div className="flexCenter flex-col bg-lightGray rounded-lg h-[180px] w-[140px] sm:h-[216px] sm:w-[160px] shadow-[0_4px_4px_0px_#0000000d]">
+              <div className="flexCenter flex-col bg-lightGray rounded-lg h-36 w-32 xs:h-[180px] xs:w-[140px] sm:h-[216px] sm:w-[160px] shadow-[0_4px_4px_0px_#0000000d]">
                 <img
                   src={category.url}
                   alt={category.name}
-                  className="h-[71px] w-[60px] object-contain object-center transition-all duration-300 ease-in-out hover:scale-110 "
+                  className="w-14 h-14 xs:h-[71px] xs:w-[60px] object-contain object-center transition-all duration-300 ease-in-out hover:scale-110 "
                 />
-                <span className="h3-bold tracking-[0.3px] mt-[13px] text-center">
+                <span className="text-xl xs:h3-bold tracking-[0.3px] mt-[13px] text-center">
                   {category.name}
                 </span>
               </div>
