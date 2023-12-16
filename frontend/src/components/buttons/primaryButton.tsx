e4868b520e8b26db7a0c51c693ae53e3ef580771
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-16 17:11:24 +0530

    Responsive the primary button

--- frontend/src/components/buttons/primaryButton.tsx
+++ frontend/src/components/buttons/primaryButton.tsx
@@ -3,7 +3,7 @@ import { CustomButtonProps } from "../../types/index";
 const PrimaryButton: React.FC<CustomButtonProps> = ({ text, style }) => {
   return (
     <button
-      className={`bg-customGreen text-white text-4xl h-10 px-[72px] rounded-md ${style}`}
+      className={`bg-customGreen text-white text-3xl h-9 px-12 sm:text-4xl sm:h-10 sm:px-[72px] rounded-md ${style}`}
     >
       {text}
     </button>
