0c2b42b1a65a5b8270382639bd84bc5f35773900
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-17 12:16:57 +0530

    Animation for service section

--- frontend/src/pages/client/home/Services.tsx
+++ frontend/src/pages/client/home/Services.tsx
@@ -3,6 +3,8 @@ import s1 from "../../../../public/images/service-img/Service 1.png";
 import s2 from "../../../../public/images/service-img/Service 2.png";
 import s3 from "../../../../public/images/service-img/Service 3.png";
 import s4 from "../../../../public/images/service-img/Service 4.png";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../utils/variants";
 
 const serviceData = [
   {
@@ -35,17 +37,38 @@ const Services = () => {
   return (
     <div className="bg-gradient-to-t from-[#53B17540] from-10% to-white h-[580px] xs:h-[400px] md:h-[450px] lg:h-[504px] px-3 w-full mx-auto mt-[36px]">
       <div className="flex-col flexCenter">
-        <h2 className="font-semibold text-[24px] text-center">Our Services</h2>
-        <p className="text-center max-w-[720px] mx-auto text-gray mt-[32px] mb-10 md:mb-16">
+        <motion.h2
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.2 }}
+          className="font-semibold text-[24px] text-center"
+        >
+          Our Services
+        </motion.h2>
+        <motion.p
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.2 }}
+          className="text-center max-w-[720px] mx-auto text-gray mt-[32px] mb-10 md:mb-16"
+        >
           Elevate your grocery shopping experience with our user-friendly
           platform, offering a seamless online shopping journey from start to
           finish. Browse through our extensive selection of fresh produce,
           organic goods, and everyday essentials—all from the comfort of your
           home.
-        </p>
+        </motion.p>
         <div className="grid items-center justify-center grid-cols-2 grid-rows-2 mt-5 text-center xs:grid-cols-4 xs:grid-rows-4 gap-x-5 gap-y-7 md:mt-3 lg:mt-6">
           {serviceData.map((service) => (
-            <div key={service.id} className="flex-col flexCenter">
+            <motion.div
+              variants={fadeIn("right", service.id * 0.2)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.2 }}
+              key={service.id}
+              className="flex-col flexCenter"
+            >
               <img
                 className="w-[55px] h-[55px] sm:w-[88px] sm:h-[88px]"
                 src={service.image}
@@ -57,7 +80,7 @@ const Services = () => {
               <p className="hidden text-xs text-gray md:block">
                 {service.description}
               </p>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
