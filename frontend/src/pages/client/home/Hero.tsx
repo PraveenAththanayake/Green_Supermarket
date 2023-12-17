4d0687198328b882729250c837da27a063977537
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-17 18:59:10 +0530

    Updated repsonsive design of hero for mobile view

--- frontend/src/pages/client/home/Hero.tsx
+++ frontend/src/pages/client/home/Hero.tsx
@@ -5,57 +5,90 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="max-w-[1120px] mx-auto relative">
-      <div className="absolute left-5 top-4 md:left-[64px] md:top-[40px] w-7xl">
-        <motion.p
-          variants={fadeIn("up", 0.2)}
-          initial="hidden"
-          animate="show"
-          className="text-xs font-bold md:text-base"
-        >
-          WELCOME GREEN SUPER MARKET ONLINE!
-        </motion.p>
-        <motion.h1
-          variants={fadeIn("up", 0.4)}
-          initial="hidden"
-          animate="show"
-          className="font-nunito font-bold mt-[32px] mb-5 md:mb-24 text-6xl md:text-8xl"
-        >
-          Fresh and Healthy <br />
-          Veggies{" "}
-          <span className="font-light">
-            Green <br />
-            Market
-          </span>
-        </motion.h1>
+    <div className="flex-col gap-10 flexBetween">
+      <div className="container min-w-[280px] min-h-[160px] max-h-[487px] max-w-[1120px] m-auto relative px-3 py-2">
+        <div className="absolute left-6 top-4 sm:left-10 md:left-16 sm:top-[40px] w-7xl">
+          <motion.p
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            className="text-[10px] font-semibold xs:text-base"
+          >
+            WELCOME GREEN SUPER MARKET ONLINE!
+          </motion.p>
+          <motion.h1
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            animate="show"
+            className="font-nunito font-bold mt-[16px] text-lg xs:text-5xl sm:text-5xl md:text-6xl lg:text-8xl"
+          >
+            Fresh and Healthy <br />
+            Veggies{" "}
+            <span className="font-light">
+              Green <br />
+              Market
+            </span>
+          </motion.h1>
+          <motion.p
+            variants={fadeIn("up", 0.6)}
+            initial="hidden"
+            animate="show"
+            className="text-[8px] font-normal text-gray mt-[3px] block xs:hidden"
+          >
+            Enjoy a seamless online shopping <br />
+            experience with just a <br />
+            few clicks.
+          </motion.p>
+          <motion.div
+            variants={fadeIn("up", 0.6)}
+            initial="hidden"
+            animate="show"
+            className="absolute hidden gap-6 mt-3 text-sm xs:mt-5 sm:mt-8 md:mt-8 lg:mt-20 xl:mt-24 md:text-4xl xs:flex"
+          >
+            <Link
+              className="flexCenter w-32 h-9 md:w-[192px] md:h-[48px] bg-customGreen border-2 border-solid border-customGreen text-white rounded-[10px] hover:bg-darkerGreen"
+              to=""
+            >
+              Shop Now
+            </Link>
+            <Link
+              className="flexCenter w-32 h-9 md:w-[192px] md:h-[48px] border-2 border-solid border-[#53B176] rounded-[10px] hover:bg-customGreen hover:text-white"
+              to=""
+            >
+              See All Products
+            </Link>
+          </motion.div>
+        </div>
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          src={heroImage}
+          alt="heroImage"
+          className="object-cover w-full min-h-[160px] min-w-[280px] h-full"
+        />
+      </div>
+      <div className="w-full flexCenter xs:hidden mb-11">
         <motion.div
           variants={fadeIn("up", 0.6)}
           initial="hidden"
           animate="show"
-          className="absolute flex gap-6 text-sm md:text-4xl"
+          className="absolute flex flex-col gap-2 text-sm font-normal"
         >
           <Link
-            className="flexCenter w-32 h-9 md:w-[192px] md:h-[48px] bg-customGreen border-2 border-solid border-customGreen text-white rounded-[10px] hover:bg-darkerGreen"
+            className="flexCenter w-[192px] h-8 md:w-[192px] md:h-[48px] bg-customGreen border-2 border-solid border-customGreen text-white rounded-[30px] hover:bg-darkerGreen"
             to=""
           >
             Shop Now
           </Link>
           <Link
-            className="flexCenter w-32 h-9 md:w-[192px] md:h-[48px] border-2 border-solid border-[#53B176] rounded-[10px] hover:bg-customGreen hover:text-white"
+            className="flexCenter w-[192px] h-8 md:w-[192px] md:h-[48px] border-2 border-solid border-[#53B176] rounded-[30px] hover:bg-customGreen hover:text-white"
             to=""
           >
             See All Products
           </Link>
         </motion.div>
       </div>
-      <motion.img
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        src={heroImage}
-        alt="heroImage"
-        className="h-[300px] w-[480px] xs:w-[768px] md:w-[1120px] md:h-[487px] lg:w-[1120px]  object-cover"
-      />
     </div>
   );
 };
