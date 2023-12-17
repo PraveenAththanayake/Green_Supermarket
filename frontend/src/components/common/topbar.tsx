dfe2a68aebe3b09ad6c39012d42f63e9a9f6b339
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-17 16:33:27 +0530

    Updated repsonsive design og topbar for mobile view

--- frontend/src/components/common/topbar.tsx
+++ frontend/src/components/common/topbar.tsx
@@ -1,55 +1,46 @@
 import { TiLocation } from "react-icons/ti";
-import { IoCall } from "react-icons/io5";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../utils/variants";
+
 const Topbar = () => {
   return (
     <motion.div
       variants={fadeIn("down", 0.2)}
       initial="hidden"
       animate="show"
-      className="h-8 text-xs xs:h-14 w-full bg-[#09965D] rounded-b-[20px] px-[20px] flexBetween mx-auto xs:text-base lg:px-[100px] xl:px-[160px]"
+      className="h-10 lg:h-14 w-full bg-[#09965D] rounded-b-[20px] px-[20px] flex items-center xs:justify-between justify-center mx-auto lg:px-[100px] xl:px-[160px] flex-col xs:flex-row gap-y-[3px]"
     >
       <motion.div
         variants={fadeIn("down", 0.2)}
         initial="hidden"
         animate="show"
       >
-        <div className="hidden xs:block">
-          <div className="flex items-center">
-            <p className="hidden md:block">Delivery from: </p>
-            <span className="mx-1 opacity-50">
-              <TiLocation />
-            </span>
-            <span className="text-white">64 Front Street, 11, Colombo...</span>
-          </div>
+        <div className="flex items-center text-[10px] xs:text-[10px] lg:text-base">
+          Delivery to:&nbsp;
+          <span className="mx-1 opacity-50">
+            <TiLocation />
+          </span>
+          <span className="text-white">64 Front Street, 11, Colombo...</span>
         </div>
-        <p className="cursor-pointer xs:hidden text-white/90 hover:scale-110">
-          <TiLocation />
-        </p>
       </motion.div>
+
       <motion.p
         variants={fadeIn("down", 0.2)}
         initial="hidden"
         animate="show"
-        className="text-white xs:hidden md:block"
+        className="hidden text-[10px] text-white sm:block lg:text-base"
       >
         Hi! Chamindu
       </motion.p>
+
       <motion.div
         variants={fadeIn("down", 0.2)}
         initial="hidden"
         animate="show"
+        className="flex-row flexCenter text-[8px] xs:text-[10px] lg:text-base"
       >
-        <div className="flex-row flexCenter">
-          <p className="hidden md:block">
-            Call Us for any query or help:&nbsp;
-          </p>
-          <span className="hidden text-white xs:block">(011) 2696523</span>
-        </div>
-        <p className="cursor-pointer hover:scale-110 xs:hidden text-white/90">
-          <IoCall />
-        </p>
+        <p>Call Us for any query or help:&nbsp;</p>
+        <span className="text-white">(011) 2696523</span>
       </motion.div>
     </motion.div>
   );
