c64e25cf8dcc75a70624bfe0c1a762762611cc04
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-18 15:57:37 +0530

    Connected to the sql server and created entity

---
+++ backend/src/main/java/com/greensupermarket/backend/entity/Product.java
@@ -0,0 +1,56 @@
+package com.greensupermarket.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "slug", nullable = false, unique = true)
+    private String slug;
+
+    @Column(name = "tag")
+    private String tag;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @Column(name = "measurements")
+    private int measurements;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "second_price")
+    private double secondPrice;
+
+    @Column(name = "unit")
+    private int unit;
+
+    @Column(name = "status")
+    private String status;
+}
