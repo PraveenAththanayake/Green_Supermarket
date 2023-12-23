f14cb072dc6739685c0ec9915969f4b4bb4140a1
chamindu <chamindulgamage@gmail.com>
2023-12-23 18:39:43 +0530

    Implement Login And Register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import CategoryDetails from "./pages/client/categoryItems/CategoryDetails";
 import ProductPage from "./pages/client/product/ProductPage";
 import { lazy, Suspense } from "react";
 import CategoryPage from "./pages/client/category/categoryPage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
 const AdminLayout = lazy(() => import("./pages/admin/AdminLayout"));
 const TopSales = lazy(() => import("./pages/client/topsales/TopSales"));
@@ -26,6 +28,8 @@ function App() {
       <Suspense fallback={<LoadingSpinner />}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/category" element={<CategoryPage />} />
           <Route path="/category/:categoryId" element={<CategoryDetails />} />
           <Route
---
+++ frontend/src/pages/auth/Login.tsx
@@ -0,0 +1,69 @@
+import leftBG from "../../../public/images/Login/LogingVector.png";
+import { FaGoogle } from "react-icons/fa";
+
+const Login = () => {
+  return (
+    <>
+      <div className="flex justify-between max-h-screen">
+        <div className="flex-1 px-[50px]">
+          <h1 className="font-nunito font-extrabold text-[24px] mt-[43px]">
+            <span className="text-[#09965D]">GREEN</span> SUPERMARKET
+          </h1>
+          <div className="flex justify-center items-center mt-[137px]">
+            <div className="w-[406px]">
+              <h2 className="font-nunito font-bold text-[48px] text-center">
+                Hi there!
+              </h2>
+              <p className="text-center mb-[32px]">
+                Wellcome to green supermarket
+              </p>
+              <button className="flex items-center justify-center border border-[#84848220] w-full h-[44px] text-[15px] text-[#848482] font-light rounded-[5px] mb-[80px]">
+                <FaGoogle color="black" className="mr-[20px]" />
+                Log in with Google
+              </button>
+              <input
+                className="border border-[#84848220] w-full h-[44px] text-[15px] text-[#848482] font-light rounded-[5px] outline-0 py-2 px-3 mb-[12px]"
+                type="text"
+                placeholder="Email address"
+              />
+              <input
+                className="border border-[#84848220] w-full h-[44px] text-[15px] text-[#848482] font-light rounded-[5px] outline-0 py-2 px-3 mb-[12px]"
+                type="text"
+                placeholder="Password"
+              />
+              <a
+                href="#"
+                className="inline-block text-[14px] text-[#848482] ml-[300px]"
+              >
+                Forgot password
+              </a>
+              <button className="w-full mt-[83px] h-[56px] bg-[#53B176] text-[20px] font-bold text-white rounded-[50px] mb-[12px]">
+                Log in
+              </button>
+              <p className="font-medium text-[14px] text-[#848482] text-center">
+                Don't have an account?{" "}
+                <a className="text-black font-bold" href="/register">
+                  Sign up
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-1 justify-end relative">
+          <img
+            className="max-w-[704px] w-full h-screen object-fill"
+            src={leftBG}
+            alt="left-bg"
+          />
+          <div className="text-center absolute top-[320px] right-[120px] leading-[80px]">
+            <h3 className="font-nunito text-[64px]">Welcome!</h3>
+            <h3 className="font-nunito text-[48px]">back to</h3>
+            <h3 className="font-nunito text-[48px]">GREEN Supermarket!</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
