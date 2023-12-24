454a9843f2e06267ed2a3e51df006cc500e7c077
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-24 15:04:06 +0530

    User Repository and RoleRepository

---
+++ backend/src/main/java/com/greensupermarket/backend/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.greensupermarket.backend.repository;
+
+import com.greensupermarket.backend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ backend/src/main/java/com/greensupermarket/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.greensupermarket.backend.repository;
+
+import com.greensupermarket.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+}
