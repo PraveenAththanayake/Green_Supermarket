7efedb8fd76a0002d0b8703bc4c1396a709e45db
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-12-24 14:55:54 +0530

    Created user and role jpa entities

---
+++ backend/src/main/java/com/greensupermarket/backend/entity/Role.java
@@ -0,0 +1,20 @@
+package com.greensupermarket.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
